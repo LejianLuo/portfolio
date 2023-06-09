3e3a7e065889c15ed9fc1446c42a7693ce824cb1
Lejian Luo <luolejian@outlook.com>
2023-06-09 01:17:08 -0400

    changed background

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Project from "./component/Project";
 
 function App() {
   return (
-    <div className="App font-gillsans bg-green-100">
+    <div className="App font-gillsans" style={{backgroundImage:`url('https://static.vecteezy.com/system/resources/thumbnails/006/941/084/small/abstract-small-hexagon-or-honeycomb-seamless-pattern-on-white-color-background-free-vector.jpg')`}}>
         <Navigation/> 
         <Landing/>
         <Project/>
