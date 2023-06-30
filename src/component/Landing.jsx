dcf79b917db3ef353f40b706c6cb3f96a2fd8d73
Lejian Luo <luolejian@outlook.com>
2023-06-30 04:01:25 -0400

    fixed style

--- src/component/Landing.jsx
+++ src/component/Landing.jsx
@@ -4,7 +4,7 @@ export default function Landing(){
     return(
         <div id='Landing' className='mx-auto relative -top-[50px]'> 
             <div className='w-[310px] md:w-[600px] lg:w-[800px] mx-auto py-[150px]'>
-                <h1 className='animate-textEnlarge text-5xl my-[20px] font-black text-purple-600'>Hello, I am Lejian</h1>
+                <h1 className='text-5xl my-[20px] font-black text-purple-600'>Hello, I am Lejian</h1>
                 <p className='text-gray-500 mb-[30px]'> As a web developer, I enjoy the process of coding, tuning, and deploying web apps. I have experience building web apps with <strong>React, ExpressJS, and MongoDB</strong>. 
                     I aim to learn a wide variety of frameworks and languages, and work as a fullstack developer in the future.</p>
                 <div className='flex gap-[50px] justify-start'>
@@ -31,27 +31,27 @@ export function Navigation(){
 function Skill(){
 
     return(
-            <div  className='group mx-auto mt-[40px] gap-y-[20px] md:gap-y-[0px] flex md:flex-row flex-col items-center md:justify-around animate-fadeIn bg-purple-100 p-[20px] rounded-2xl border-4 border-slate-100'>
+            <div  className='mx-auto mt-[40px] gap-y-[20px] md:gap-y-[0px] flex md:flex-row flex-col items-center md:justify-around bg-purple-100 p-[20px] rounded-2xl border-4 border-slate-100'>
                 <div>
                     <div className='flex gap-[5px] justify-between w-[120px] '>
                         <h1 className='font-bold text-gray-600 text-lg'>Language</h1>
                         <img className='w-[20px]' src='https://www.nicepng.com/png/detail/468-4682992_png-file-svg-programming-languages-png-icon.png' alt='img'/>
                     </div>
                     <div className='flex gap-[5px] justify-between w-[120px] my-[10px]'>
-                        <h1 className='font-thin text-gray-400 text-sm group-hover:text-gray-600 group-hover:font-bold'>HTML</h1>
-                        <img className='w-[20px] opacity-40 group-hover:opacity-100' src='https://cdn-icons-png.flaticon.com/512/732/732212.png' alt='img'/>
+                        <h1 className='text-sm text-gray-600 font-bold'>HTML</h1>
+                        <img className='w-[20px] opacity-100' src='https://cdn-icons-png.flaticon.com/512/732/732212.png' alt='img'/>
                     </div>
                     <div className='flex gap-[5px] justify-between w-[120px] my-[10px]'>
-                        <h1 className='font-thin text-gray-400 text-sm group-hover:text-gray-600 group-hover:font-bold'>CSS</h1>
-                        <img className='w-[20px] opacity-40 group-hover:opacity-100' src='https://cdn-icons-png.flaticon.com/512/5968/5968242.png' alt='img'/>
+                        <h1 className='text-sm text-gray-600 font-bold'>CSS</h1>
+                        <img className='w-[20px] opacity-100' src='https://cdn-icons-png.flaticon.com/512/5968/5968242.png' alt='img'/>
                     </div>
                     <div className='flex gap-[5px] justify-between w-[120px] my-[10px]'>
-                        <h1 className='font-thin text-gray-400 text-sm group-hover:text-gray-600 group-hover:font-bold'>JAVASCRIPT</h1>
-                        <img className='w-[20px] opacity-40 group-hover:opacity-100' src='https://e7.pngegg.com/pngimages/793/545/png-clipart-javascript-logo-computer-icons-vue-js-angle-text-thumbnail.png' alt='img'/>
+                        <h1 className='text-sm text-gray-600 font-bold'>JAVASCRIPT</h1>
+                        <img className='w-[20px] opacity-100' src='https://e7.pngegg.com/pngimages/793/545/png-clipart-javascript-logo-computer-icons-vue-js-angle-text-thumbnail.png' alt='img'/>
                     </div>
                     <div className='flex gap-[5px] justify-between w-[120px] my-[10px]'>
-                        <h1 className='font-thin text-gray-400 text-sm group-hover:text-gray-600 group-hover:font-bold'>JAVA</h1>
-                        <img className='w-[20px] opacity-40 group-hover:opacity-100' src='https://cdn-icons-png.flaticon.com/512/226/226777.png' alt='img'/>
+                        <h1 className='text-sm text-gray-600 font-bold'>JAVA</h1>
+                        <img className='w-[20px] opacity-100' src='https://cdn-icons-png.flaticon.com/512/226/226777.png' alt='img'/>
                     </div>
                 </div>
                 <div>
@@ -60,20 +60,20 @@ function Skill(){
                         <img className='w-[20px]' src='https://cdn-icons-png.flaticon.com/512/2721/2721616.png' alt='img'/>
                     </div>
                     <div className='flex gap-[5px] justify-between w-[120px] my-[10px]'>
-                        <h1 className='font-thin text-gray-400 text-sm group-hover:text-gray-600 group-hover:font-bold'>REACT</h1>
-                        <img className='w-[20px] opacity-40 group-hover:opacity-100' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1150px-React-icon.svg.png' alt='img'/>
+                        <h1 className='text-sm text-gray-600 font-bold'>REACT</h1>
+                        <img className='w-[20px] opacity-100' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1150px-React-icon.svg.png' alt='img'/>
                     </div>
                     <div className='flex gap-[5px] justify-between w-[120px] my-[10px]'>
-                        <h1 className='font-thin text-gray-400 text-sm group-hover:text-gray-600 group-hover:font-bold'>TAILWIND</h1>
-                        <img className='w-[20px] opacity-40 group-hover:opacity-100' src='https://www.drupal.org/files/styles/grid-3-2x/public/project-images/screenshot_361.png?itok=w4CzcWyb' alt='img'/>
+                        <h1 className='text-sm text-gray-600 font-bold'>TAILWIND</h1>
+                        <img className='w-[20px] opacity-100' src='https://www.drupal.org/files/styles/grid-3-2x/public/project-images/screenshot_361.png?itok=w4CzcWyb' alt='img'/>
                     </div>
                     <div className='flex gap-[5px] justify-between w-[120px] my-[10px]'>
-                        <h1 className='font-thin text-gray-400 text-sm group-hover:text-gray-600 group-hover:font-bold'>BOOTSTRAP</h1>
-                        <img className='w-[20px] opacity-40 group-hover:opacity-100' src='https://cdn-icons-png.flaticon.com/512/5968/5968672.png' alt='img'/>
+                        <h1 className='text-sm text-gray-600 font-bold'>BOOTSTRAP</h1>
+                        <img className='w-[20px] opacity-100' src='https://cdn-icons-png.flaticon.com/512/5968/5968672.png' alt='img'/>
                     </div>
                     <div className='flex gap-[5px] justify-between w-[120px] my-[10px]'>
-                        <h1 className='font-thin text-gray-400 text-sm group-hover:text-gray-600 group-hover:font-bold'>MATERIAL UI</h1>
-                        <img className='w-[20px] opacity-40 group-hover:opacity-100' src='https://v4.material-ui.com/static/logo.png' alt='img'/>
+                        <h1 className='text-sm text-gray-600 font-bold'>MATERIAL UI</h1>
+                        <img className='w-[20px] opacity-100' src='https://v4.material-ui.com/static/logo.png' alt='img'/>
                     </div>
                 </div>
                 <div>
@@ -82,20 +82,20 @@ function Skill(){
                         <img className='w-[20px]' src='https://cdn-icons-png.flaticon.com/512/657/657695.png' alt='img'/>
                     </div>
                     <div className='flex gap-[5px] justify-between w-[120px] my-[10px]'>
-                        <h1 className='font-thin text-gray-400 text-sm group-hover:text-gray-600 group-hover:font-bold'>NODEJS</h1>
-                        <img className='w-[20px] opacity-40 group-hover:opacity-100' src='https://cdn-icons-png.flaticon.com/512/5968/5968322.png' alt='img'/>
+                        <h1 className='text-sm text-gray-600 font-bold'>NODEJS</h1>
+                        <img className='w-[20px] opacity-100' src='https://cdn-icons-png.flaticon.com/512/5968/5968322.png' alt='img'/>
                     </div>
                     <div className='flex gap-[5px] justify-between w-[120px] my-[10px]'>
-                        <h1 className='font-thin text-gray-400 text-sm group-hover:text-gray-600 group-hover:font-bold'>EXPRESSJS</h1>
-                        <img className='w-[20px] opacity-40 group-hover:opacity-100' src='https://youteam.io/blog/wp-content/uploads/2022/04/expressjs_logo.png' alt='img'/>
+                        <h1 className='text-sm text-gray-600 font-bold'>EXPRESSJS</h1>
+                        <img className='w-[20px] opacity-100' src='https://youteam.io/blog/wp-content/uploads/2022/04/expressjs_logo.png' alt='img'/>
                     </div>
                     <div className='flex gap-[5px] justify-between w-[120px] my-[10px]'>
-                        <h1 className='font-thin text-gray-400 text-sm group-hover:text-gray-600 group-hover:font-bold'>MONGODB</h1>
-                        <img className='w-[20px] opacity-40 group-hover:opacity-100' src='https://www.opc-router.de/wp-content/uploads/2021/03/mongodb_thumbnail.png' alt='img'/>
+                        <h1 className='text-sm text-gray-600 font-bold'>MONGODB</h1>
+                        <img className='w-[20px] opacity-100' src='https://www.opc-router.de/wp-content/uploads/2021/03/mongodb_thumbnail.png' alt='img'/>
                     </div>
                     <div className='flex gap-[5px] justify-between w-[120px] my-[10px]'>
-                        <h1 className='font-thin text-gray-400 text-sm group-hover:text-gray-600 group-hover:font-bold'>MONGOOSE</h1>
-                        <img className='w-[20px] opacity-40 group-hover:opacity-100' src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongoose/mongoose.png' alt='img'/>
+                        <h1 className='text-sm text-gray-600 font-bold'>MONGOOSE</h1>
+                        <img className='w-[20px] opacity-100' src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongoose/mongoose.png' alt='img'/>
                     </div>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,20 +8,7 @@ module.exports = {
       fontFamily: {
         gillsans: ["Gill Sans",'sans-serif'],
       },
-      keyframes: {
-        fadeIn: {
-          'from': { opacity: 0 },
-          'to': { opacity: 1 },
-        },
-        textEnlarge: {
-          'from': { fontSize: '2rem' },
-          'to': { fontSize: '3rem' },
-        },
-      },
-      animation: {
-        'fadeIn': 'fadeIn 1.5s ease-in-out',
-        'textEnlarge':'textEnlarge 1.5s ease-in-out' 
-      }
+
     },
   },
   plugins: [],
